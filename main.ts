ac0921318d452d2c7e70f3ee64f702e014fe8c4b
Pablo Montaña <135363231+pablodmconti@users.noreply.github.com>
2025-09-30 11:21:02 -0300

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-input.onPinPressed(TouchPin.P0, function () {
-    // Guardamos el número aleatorio en la variable 'numero'
+input.onButtonPressed(Button.AB, function () {
+    // Guardamos el número aleatorio en la variable 'numero'
     numero = randint(0, 100)
     basic.showNumber(numero)
-    // Comparamos la variable 'numero' para decidir qué mensaje mostrar
+    // Comparamos la variable 'numero' para decidir qué mensaje mostrar
     if (numero <= 25) {
-        basic.showString("Sin amor")
+        music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.InBackground)
+        basic.showString("" + ("Sin amor"))
     } else if (numero <= 50) {
-        basic.showString("Amor de mejores amigos")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.showString("" + ("Amor de mejores amigos"))
+        pins.digitalWritePin(DigitalPin.P0, 0)
     } else if (numero <= 75) {
-        basic.showString("Amor desconsolado")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showString("" + ("Amor desconsolado"))
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     } else {
-        basic.showString("Amor ardiente")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+        basic.showString("" + ("Amor ardiente"))
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
 let numero = 0
-basic.showString("Termometro del carino")
+basic.showString("Termómetro del amor")
